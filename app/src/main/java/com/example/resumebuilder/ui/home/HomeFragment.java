2547cb109b767b91b9880638ad870e29e18fabed
Biba2023 <drnaleks@gmail.com>
2023-04-19 15:36:24 +0700

    first

---
+++ app/src/main/java/com/example/resumebuilder/ui/home/HomeFragment.java
@@ -0,0 +1,37 @@
+package com.example.resumebuilder.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.resumebuilder.databinding.FragmentContactBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentContactBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentContactBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
